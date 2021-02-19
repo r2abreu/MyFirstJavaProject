31c95ff69b063e4055320142eb536a4d6be10c86
r2abreu <r2abreu@pm.me>
2021-02-19 20:47:24 +0100

    Student model created, overriding toString, hashCode and equals

--- test/com/netmind/business/integration/test/ArrayOperatorIntegrationTest.java
+++ test/com/netmind/business/integration/test/ArrayOperatorIntegrationTest.java
@@ -1,20 +1,21 @@
 package com.netmind.business.integration.test;
 
-import static org.junit.Assert.*;
-
-import com.netmind.business.ArrayOperator;
-
-import com.netmind.data.ArrayGenerator;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
+import com.netmind.business.ArrayOperator;
+import com.netmind.data.ArrayGenerator;
+
 public class ArrayOperatorIntegrationTest {
-	
+
 	@Test
-	public void testReducer() {
+	public void testSumEvenIndexElements() {
 		ArrayGenerator generator = new ArrayGenerator();
 		ArrayOperator operator = new ArrayOperator();
+
 		int[] arr = generator.generateNaturalSequenceArray(100);
+
 		assertEquals(operator.sumEvenIndexElements(arr), 2450);
 	}
 
